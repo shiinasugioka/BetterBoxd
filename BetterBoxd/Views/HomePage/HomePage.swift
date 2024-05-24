29378bf1baf6b0b1e108ea229e9c676af99e0b41
Shiina Sugioka <shiina12@uw.edu>
2024-05-24 14:04:55 -0700

    set up files

---
+++ BetterBoxd/Views/HomePage/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  BetterBoxd
+//
+//  Created by Shiina on 5/24/24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
---
+++ BetterBoxd/Views/Pages/NavBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavBar.swift
+//  BetterBoxd
+//
+//  Created by Shiina on 5/24/24.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavBar()
+}
