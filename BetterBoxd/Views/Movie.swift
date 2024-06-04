421c8db7a2b07cfa4aa43570ceb92f7077d2e86e
Aditya Khowal <108611217+AdityaKhowalGithub@users.noreply.github.com>
2024-06-04 02:19:21 -0700

    Update Movie.swift

--- BetterBoxd/Views/Movie.swift
+++ BetterBoxd/Views/Movie.swift
@@ -1,96 +1,96 @@
-import Foundation
-import RealmSwift
+// import Foundation
+// import RealmSwift
 
-struct Movie: Identifiable, Codable {
-    let id: Int
-    let title: String
-    let overview: String
-    let posterPath: String?
-    let releaseDate: Date?
-    let adult: Bool
-    let backdropPath: String?
-    let genreIds: [Int]
-    let originalLanguage: String
-    let originalTitle: String
-    let popularity: Double
-    let video: Bool
-    let voteAverage: Double
-    let voteCount: Int
+// struct Movie: Identifiable, Codable {
+//     let id: Int
+//     let title: String
+//     let overview: String
+//     let posterPath: String?
+//     let releaseDate: Date?
+//     let adult: Bool
+//     let backdropPath: String?
+//     let genreIds: [Int]
+//     let originalLanguage: String
+//     let originalTitle: String
+//     let popularity: Double
+//     let video: Bool
+//     let voteAverage: Double
+//     let voteCount: Int
 
-    init(
-        id: Int,
-        title: String,
-        overview: String,
-        posterPath: String? = nil,
-        releaseDate: Date? = nil,
-        adult: Bool = false,
-        backdropPath: String? = nil,
-        genreIds: [Int] = [],
-        originalLanguage: String = "en",
-        originalTitle: String = "",
-        popularity: Double = 0.0,
-        video: Bool = false,
-        voteAverage: Double = 0.0,
-        voteCount: Int = 0
-    ) {
-        self.id = id
-        self.title = title
-        self.overview = overview
-        self.posterPath = posterPath
-        self.releaseDate = releaseDate
-        self.adult = adult
-        self.backdropPath = backdropPath
-        self.genreIds = genreIds
-        self.originalLanguage = originalLanguage
-        self.originalTitle = originalTitle
-        self.popularity = popularity
-        self.video = video
-        self.voteAverage = voteAverage
-        self.voteCount = voteCount
-    }
+//     init(
+//         id: Int,
+//         title: String,
+//         overview: String,
+//         posterPath: String? = nil,
+//         releaseDate: Date? = nil,
+//         adult: Bool = false,
+//         backdropPath: String? = nil,
+//         genreIds: [Int] = [],
+//         originalLanguage: String = "en",
+//         originalTitle: String = "",
+//         popularity: Double = 0.0,
+//         video: Bool = false,
+//         voteAverage: Double = 0.0,
+//         voteCount: Int = 0
+//     ) {
+//         self.id = id
+//         self.title = title
+//         self.overview = overview
+//         self.posterPath = posterPath
+//         self.releaseDate = releaseDate
+//         self.adult = adult
+//         self.backdropPath = backdropPath
+//         self.genreIds = genreIds
+//         self.originalLanguage = originalLanguage
+//         self.originalTitle = originalTitle
+//         self.popularity = popularity
+//         self.video = video
+//         self.voteAverage = voteAverage
+//         self.voteCount = voteCount
+//     }
 
-    var posterURL: URL? {
-        if let path = posterPath {
-            return URL(string: "https://image.tmdb.org/t/p/w500\(path)")
-        }
-        return nil
-    }
+//     var posterURL: URL? {
+//         if let path = posterPath {
+//             return URL(string: "https://image.tmdb.org/t/p/w500\(path)")
+//         }
+//         return nil
+//     }
 
-    enum CodingKeys: String, CodingKey {
-        case id
-        case title
-        case overview
-        case posterPath = "poster_path"
-        case releaseDate = "release_date"
-        case adult
-        case backdropPath = "backdrop_path"
-        case genreIds = "genre_ids"
-        case originalLanguage = "original_language"
-        case originalTitle = "original_title"
-        case popularity
-        case video
-        case voteAverage = "vote_average"
-        case voteCount = "vote_count"
-    }
-}
+//     enum CodingKeys: String, CodingKey {
+//         case id
+//         case title
+//         case overview
+//         case posterPath = "poster_path"
+//         case releaseDate = "release_date"
+//         case adult
+//         case backdropPath = "backdrop_path"
+//         case genreIds = "genre_ids"
+//         case originalLanguage = "original_language"
+//         case originalTitle = "original_title"
+//         case popularity
+//         case video
+//         case voteAverage = "vote_average"
+//         case voteCount = "vote_count"
+//     }
+// }
 
-struct MovieResponse: Codable {
-    let dates: Dates?
-    let page: Int
-    let results: [Movie]
-    let totalPages: Int
-    let totalResults: Int
+// struct MovieResponse: Codable {
+//     let dates: Dates?
+//     let page: Int
+//     let results: [Movie]
+//     let totalPages: Int
+//     let totalResults: Int
     
-    enum CodingKeys: String, CodingKey {
-        case dates
-        case page
-        case results
-        case totalPages = "total_pages"
-        case totalResults = "total_results"
-    }
-}
+//     enum CodingKeys: String, CodingKey {
+//         case dates
+//         case page
+//         case results
+//         case totalPages = "total_pages"
+//         case totalResults = "total_results"
+//     }
+// }
 
-struct Dates: Codable {
-    let maximum: String
-    let minimum: String
-}
+// struct Dates: Codable {
+//     let maximum: String
+//     let minimum: String
+// }
