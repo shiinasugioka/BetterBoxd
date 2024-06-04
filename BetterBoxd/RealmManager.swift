b06bb3bc5909b79a057b09a8fbc590b1ae8f25f6
Shiina Sugioka <shiina12@uw.edu>
2024-06-04 00:56:22 -0700

    realm works

--- BetterBoxd/RealmManager.swift
+++ BetterBoxd/RealmManager.swift
@@ -11,61 +11,26 @@ import Foundation
 class RealmManager {
     static let shared = RealmManager()
     let app: RealmSwift.App?
-    var localRealm: Realm? // until auth0 is set up
     
     private init() {
         // use this to connect to realm studio
         let realm = try! Realm()
         print("Realm is located at:", realm.configuration.fileURL!)
         
-        if let appId = Bundle.main.object(forInfoDictionaryKey: "REALM_APP_ID") as? String {
-            self.app = RealmSwift.App(id: appId)
-        } else {
-            self.app = nil
-            setupLocalRealm()
-        }
+        let appId = Bundle.main.object(forInfoDictionaryKey: "REALM_APP_ID") as! String
+        self.app = RealmSwift.App(id: appId)
     }
     
     func getConfiguration() -> Realm.Configuration {
-        if let user = app?.currentUser {
-            return user.configuration(partitionValue: "user=\(user.id)")
-        } else {
-            return localRealmConfiguration()
-        }
+        return Realm.Configuration()
     }
-    
-    private func localRealmConfiguration() -> Realm.Configuration {
-        var config = Realm.Configuration()
-        config.inMemoryIdentifier = "LocalRealm"
-        return config
-    }
-    
-    private func setupLocalRealm() {
-        let config = localRealmConfiguration()
-        do {
-            localRealm = try Realm(configuration: config)
-        } catch {
-            fatalError("Failed to open local Realm: \(error.localizedDescription)")
-        }
-    }
+
     
     func addUser(name: String, email: String) {
-        guard let localRealm = localRealm else {
-            print("Local Realm is not set up")
-            return
-        }
-        
         let user = User()
         user.name = name
         user.email = email
         
-        do {
-            try localRealm.write {
-                localRealm.add(user)
-            }
-            print("User added successfully")
-        } catch {
-            print("Failed to add user: \(error.localizedDescription)")
-        }
+        print("User added successfully")
     }
 }
